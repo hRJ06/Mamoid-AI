8f8c229e948bf628aa36ae93373d3a838ffd5a4c
hRJ06 <hindolroy.2306@gmail.com>
2024-10-05 19:51:55 +0530

    feat: add API route for Aurinko

---
+++ src/lib/aurinko.ts
@@ -0,0 +1,72 @@
+"use server";
+import axios from "axios";
+import { auth } from "@clerk/nextjs/server";
+
+/* OAUTH FLOW TO GET AURINKO CODE */
+export const getAurinkoAuthUrl = async (
+  serviceType: "Google" | "Office365",
+) => {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+  const params = new URLSearchParams({
+    clientId: process.env.AURIKO_CLIENT_ID as string,
+    serviceType,
+    scopes: "Mail.Read Mail.ReadWrite Mail.Send Mail.Drafts Mail.All",
+    responseType: "code",
+    returnUrl: `${process.env.NEXT_PUBLIC_URL}/api/aurinko/callback`,
+  });
+  return `https://api.aurinko.io/v1/auth/authorize?${params.toString()}`;
+};
+
+/* EXCHANGE RETURN URL CODE FOR TOKEN */
+export const exchangeCodeForAccessToken = async (code: string) => {
+  try {
+    const response = await axios.post(
+      `https://api.aurinko.io/v1/auth/token/${code}`,
+      {},
+      {
+        auth: {
+          username: process.env.AURINKO_CLIENT_ID as string,
+          password: process.env.AURINKO_CLIENT_SECRET as string,
+        },
+      },
+    );
+    return response.data as {
+      accountId: number;
+      accessToken: string;
+      userId: string;
+      userSession: string;
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        error.response?.data?.error ||
+          "Failed to exchange code for access Token",
+      );
+    }
+    console.error(error);
+  }
+};
+
+/* GET ACCOUNT DETAILS FROM TOKEN */
+export const getAccountDetails = async (accessToken: string) => {
+  try {
+    const response = await axios.get("https://api.aurinko.io/v1/account", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.data as {
+      email: string;
+      name: string;
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        error.response?.data?.error ||
+          "Failed to exchange code for access Token",
+      );
+    }
+    console.error(error);
+  }
+};
